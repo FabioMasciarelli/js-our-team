6a8c755902570c8a81947b34551e0f84664a3846
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-11 15:06:17 +0200

    add: first milestone

---
+++ README.md
@@ -0,0 +1,8 @@
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+Creare l’array di oggetti con le informazioni fornite.
+
+Stampare su console le informazioni di nome, ruolo e la stringa della foto per ogni membro del team
+
+Stampare le stesse informazioni su DOM sottoforma di stringhe 
---
+++ js/script.js
@@ -0,0 +1,37 @@
+// creare array di oggetti con le informazioni fornite
+
+const profile = [
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        profileImg: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        profileImg: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        profileImg: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        profileImg: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        profileImg: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        profileImg: "barbara-ramos-graphic-designer.jpg", 
+    }
+];
+
+
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto per ogni membro del team
